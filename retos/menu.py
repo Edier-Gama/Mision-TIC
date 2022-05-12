de178848ecf24db9c0b01cbaef6f9d39019a3613
Edier Gama <ediersantiago7@gmail.com>
2022-05-12 18:34:46 -0500

    creacion de la funcion menu y mejoras de tipeo al inicio de sesion

---
+++ retos/menu.py
@@ -0,0 +1,21 @@
+def menu ():
+
+ var_menu = input("""
+   Bienvenido, elija una de las siguientes opciones: 
+         
+    1 - Adivinanza
+    2 - Cerrar sesión
+
+""")
+
+homepage = int(input(" "))
+
+if ( homepage== 1):
+    print("Khe pro soi")
+elif (homepage == 2):
+    exit()
+else: 
+    print("Digita alguna monda ombe")   
+exit()         
+
+
